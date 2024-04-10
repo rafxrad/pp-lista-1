4ed5633f6b2ec631d28565d4475e4c28e2de3d96
rafxrad <weissrafaella@gmail.com>
2024-04-10 08:49:38 -0300

    feat: new game

--- src/game/Main.java
+++ src/game/Main.java
@@ -8,12 +8,17 @@ public class Main {
 		
 		Location rio = new RioDeJaneiro();
 		Location amazonia = new Amazonia();
+		Location sertao = new Sertao();
+		
 		
 		Game mygame1 = new Game(rio);
 		Game mygame2 = new Game(amazonia);
+		Game mygame3 = new Game(sertao);
+		
 		
 		mygame1.play();
 		mygame2.play();
+		mygame3.play();
 		
 		
 
